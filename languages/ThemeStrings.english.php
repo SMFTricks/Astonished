4686b65a538ed7d1a0f043c40ebd96e7b15e5e6f
Diego Andrés <diegoandres_cortes@outlook.com>
2021-10-14 17:03:40 -0500

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0; Themes
+$txt['facebook_url'] = 'Facebook URL';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['youtube_url'] = 'Youtube URL';
+$txt['rss_url'] = 'RSS URL';
+$txt['atd_mant'] = 'Maintenance';
+$txt['atd_reports'] = 'Reports';
+$txt['yah_atd'] = 'You are Here:';
